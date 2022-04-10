08bb691b63d5aa13b891f085ec7e9019f5a77f45
Muhammet Albayrak <mhasanalbyrk@gmail.com>
2022-04-10 13:00:24 +0300

    gitignore and final state

--- run.py
+++ run.py
@@ -94,13 +94,12 @@ def rotation_matrix(degrees, axis):
         return z
 
 
-def rotate(theta, joint_number, clock_wise: bool = False):
+def rotate(theta, joint_number, clock_wise: bool = False) -> object:
     if clock_wise:
         theta = theta * -1
 
     theta_arr = np.array([0, 0, 0, 0])
     theta_arr[joint_number] = theta
-
 
     rot_1_2 = np.matmul(rotation_matrix(theta_arr[0], 'y'), R_1_2)
     rot_2_3 = np.matmul(rotation_matrix(theta_arr[1], 'z'), R_2_3)
@@ -119,6 +118,7 @@ def print_new_axes(x_arrow, y_arrow, z_arrow, rotate_end_mat):
     new_x_axis = np.matmul(rotate_end_mat, np.array([x_arrow.axis.x, x_arrow.axis.y, x_arrow.axis.z]))
     new_y_axis = np.matmul(rotate_end_mat, np.array([y_arrow.axis.x, y_arrow.axis.y, y_arrow.axis.z]))
     new_z_axis = np.matmul(rotate_end_mat, np.array([z_arrow.axis.x, z_arrow.axis.y, z_arrow.axis.z]))
+
     x_arrow.axis = vpy.vector(new_x_axis[0], new_x_axis[1], new_x_axis[2])
     y_arrow.axis = vpy.vector(new_y_axis[0], new_y_axis[1], new_y_axis[2])
     z_arrow.axis = vpy.vector(new_z_axis[0], new_z_axis[1], new_z_axis[2])
@@ -152,23 +152,28 @@ if __name__ == "__main__":
     end_frame_curve_y = vpy.arrow(length=2, pos=origin_vector, axis=end_frame_y, color=color.red)
     end_frame_curve_z = vpy.arrow(length=2, pos=origin_vector, axis=end_frame_z, color=color.red)
 
-    end_x_label = vpy.label(text="x", color=vpy.color.cyan,
+    end_x_label = vpy.label(text="x",
+                            color=vpy.color.cyan,
                             pos=end_frame_curve_x.pos + end_frame_curve_x.axis + vpy.vector(offset, 0, 0),
                             box=False)
-    end_y_label = vpy.label(text="y", color=vpy.color.cyan,
+    end_y_label = vpy.label(text="y",
+                            color=vpy.color.cyan,
                             pos=end_frame_curve_y.pos + end_frame_curve_y.axis + vpy.vector(0, offset, 0),
                             box=False)
     end_z_label = vpy.label(text="z", color=vpy.color.cyan,
                             pos=end_frame_curve_z.pos + end_frame_curve_z.axis + vpy.vector(0, 0, offset),
                             box=False)
 
-    base_x_label = vpy.label(text="x", color=vpy.color.red,
+    base_x_label = vpy.label(text="x",
+                             color=vpy.color.red,
                              pos=base_frame_curve_x.pos + base_frame_curve_x.axis + vpy.vector(offset, 0, 0),
                              box=False)
-    base_y_label = vpy.label(text="y", color=vpy.color.red,
+    base_y_label = vpy.label(text="y",
+                             color=vpy.color.red,
                              pos=base_frame_curve_y.pos + base_frame_curve_y.axis + vpy.vector(0, offset, 0),
                              box=False)
-    base_z_label = vpy.label(text="z", color=vpy.color.red,
+    base_z_label = vpy.label(text="z",
+                             color=vpy.color.red,
                              pos=base_frame_curve_z.pos + base_frame_curve_z.axis + vpy.vector(0, 0, offset),
                              box=False)
 
@@ -176,52 +181,61 @@ if __name__ == "__main__":
     print_new_axes(end_frame_curve_x, end_frame_curve_y, end_frame_curve_z, rotate_end_matrix)
     update_labels(end_x_label, end_y_label, end_z_label, end_frame_curve_x, end_frame_curve_y,
                   end_frame_curve_z)
+
     while True:
         rate(5)
         try:
             if keyboard.is_pressed('z'):
+                print('Rotating joint 1 10 degrees')
                 rotate_end_matrix = rotate(THETA, 0)
                 print_new_axes(end_frame_curve_x, end_frame_curve_y, end_frame_curve_z, rotate_end_matrix)
                 update_labels(end_x_label, end_y_label, end_z_label, end_frame_curve_x, end_frame_curve_y,
                               end_frame_curve_z)
 
             elif keyboard.is_pressed('a'):
+                print('Rotating joint 1 10 degrees')
                 rotate_end_matrix = rotate(THETA, 0, clock_wise=True)
                 print_new_axes(end_frame_curve_x, end_frame_curve_y, end_frame_curve_z, rotate_end_matrix)
                 update_labels(end_x_label, end_y_label, end_z_label, end_frame_curve_x, end_frame_curve_y,
                               end_frame_curve_z)
 
             elif keyboard.is_pressed('x'):
+                print('Rotating joint 2 10 degrees')
                 rotate_end_matrix = rotate(THETA, 1)
                 print_new_axes(end_frame_curve_x, end_frame_curve_y, end_frame_curve_z, rotate_end_matrix)
                 update_labels(end_x_label, end_y_label, end_z_label, end_frame_curve_x, end_frame_curve_y,
                               end_frame_curve_z)
 
             elif keyboard.is_pressed('s'):
+                print('Rotating joint 2 10 degrees')
                 rotate_end_matrix = rotate(THETA, 1, clock_wise=True)
                 print_new_axes(end_frame_curve_x, end_frame_curve_y, end_frame_curve_z, rotate_end_matrix)
                 update_labels(end_x_label, end_y_label, end_z_label, end_frame_curve_x, end_frame_curve_y,
                               end_frame_curve_z)
 
             elif keyboard.is_pressed('c'):
+                print('Rotating joint 3 10 degrees')
                 rotate_end_matrix = rotate(THETA, 2)
                 print_new_axes(end_frame_curve_x, end_frame_curve_y, end_frame_curve_z, rotate_end_matrix)
                 update_labels(end_x_label, end_y_label, end_z_label, end_frame_curve_x, end_frame_curve_y,
                               end_frame_curve_z)
 
             elif keyboard.is_pressed('d'):
+                print('Rotating joint 3 10 degrees')
                 rotate_end_matrix = rotate(THETA, 2, clock_wise=True)
                 print_new_axes(end_frame_curve_x, end_frame_curve_y, end_frame_curve_z, rotate_end_matrix)
                 update_labels(end_x_label, end_y_label, end_z_label, end_frame_curve_x, end_frame_curve_y,
                               end_frame_curve_z)
 
             elif keyboard.is_pressed('f'):
+                print('Rotating joint 4 10 degrees')
                 rotate_end_matrix = rotate(THETA, 3)
                 print_new_axes(end_frame_curve_x, end_frame_curve_y, end_frame_curve_z, rotate_end_matrix)
                 update_labels(end_x_label, end_y_label, end_z_label, end_frame_curve_x, end_frame_curve_y,
                               end_frame_curve_z)
 
             elif keyboard.is_pressed('v'):
+                print('Rotating joint 4 10 degrees')
                 rotate_end_matrix = rotate(THETA, 3, clock_wise=True)
                 print_new_axes(end_frame_curve_x, end_frame_curve_y, end_frame_curve_z, rotate_end_matrix)
                 update_labels(end_x_label, end_y_label, end_z_label, end_frame_curve_x, end_frame_curve_y,
