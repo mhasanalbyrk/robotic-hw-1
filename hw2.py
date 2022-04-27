89953779a2afa45e33f3f777193e4c030e92b0dd
Muhammet Albayrak <mhasanalbyrk@gmail.com>
2022-04-27 22:48:58 +0300

    finisged rotating part1

--- hw2.py
+++ hw2.py
@@ -69,19 +69,22 @@ def redraw(matrix: ndarray, part: vpy.compound, origin: vpy.vector, part_number)
     rot: ndarray[3][3]
     rot = matrix[0:3]
     rot = rot[:, [0, 1, 2]]
+    part.pos = vpy.vector(part_1.pos.x + matrix[0][3], part_1.pos.y + matrix[1][3], A_1 + matrix[2][3])
+
 
     new_x_axis = np.matmul(rot, np.array([
-        part.axis.x,
-        part.axis.y,
-        part.axis.z
+        part_1.axis.x,
+        part_1.axis.y,
+        part_1.axis.z
     ]))
 
     if part_number == 2:
-        part.rotate(angle=np.radians(-5), origin=origin, axis=vpy.vector(0, 0, 1), )
+        # part.rotate(angle=np.radians(-5), origin=origin, axis=vpy.vector(0, 0, 1), )
         # part.pos = origin
         print(f'rot for part2 = {rot}')
         # calculate new axis
         # print(part.origin)
+        part.axis = vpy.vector(new_x_axis[0], new_x_axis[1], new_x_axis[2])
 
         # new_y_axis = np.matmul(rot, np.array([part.axis.x, part.axis.y, part.axis.z]))
         # new_z_axis = np.matmul(rot, np.array([part.axis.x, part.axis.y, part.axis.z]))
@@ -91,7 +94,9 @@ def redraw(matrix: ndarray, part: vpy.compound, origin: vpy.vector, part_number)
         # part.pos.x = matrix[0][3] +3
         # part.pos.y = matrix[1][3]
         # part.pos.z = matrix[2][3]
-        part.rotate(angle=np.radians(-5), axis=vpy.vector(0, 0, 1), origin=origin)
+        # part.rotate(angle=np.radians(-5), axis=vpy.vector(0, 0, 1), origin=origin)
+        part.axis = vpy.vector(new_x_axis[0], new_x_axis[1], new_x_axis[2])
+
     end_x_label.pos = part_2.pos + vpy.vector(offset, 0, 0)
     part_3_pos.pos = part_3.pos + vpy.vector(offset, 0, 0)
 
@@ -155,30 +160,28 @@ if __name__ == "__main__":
     rod2_4: vpy.cylinder = vpy.cylinder(pos=vpy.vector(motor_2.pos.x + 3, motor_2.pos.y + 1.5, motor_2.pos.z),
                                         radius=0.2,
                                         axis=vpy.vector(3, 0, 0))
-    rod3_1: vpy.cylinder = vpy.cylinder(pos=vpy.vector(motor_2.pos.x + 3, -motor_2.pos.y, motor_2.pos.z), radius=0.2,
+    rod3_1: vpy.cylinder = vpy.cylinder(pos=vpy.vector(motor_2.pos.x + 6, -motor_2.pos.y, motor_2.pos.z), radius=0.2,
                                         axis=vpy.vector(3, 0, 0))
-    rod3_2: vpy.cylinder = vpy.cylinder(pos=vpy.vector(motor_2.pos.x + 3, motor_2.pos.y, motor_2.pos.z), radius=0.2,
+    rod3_2: vpy.cylinder = vpy.cylinder(pos=vpy.vector(motor_2.pos.x + 6, motor_2.pos.y, motor_2.pos.z), radius=0.2,
                                         axis=vpy.vector(3, 0, 0))
-    rod3_3: vpy.cylinder = vpy.cylinder(pos=vpy.vector(motor_2.pos.x + 3 + 3, motor_2.pos.y, motor_2.pos.z), radius=0.2,
+    rod3_3: vpy.cylinder = vpy.cylinder(pos=vpy.vector(motor_2.pos.x + 6 + 3, motor_2.pos.y, motor_2.pos.z), radius=0.2,
                                         axis=vpy.vector(0, 3, 0))
-    rod3_4: vpy.cylinder = vpy.cylinder(pos=vpy.vector(motor_2.pos.x + 3 + 3, motor_2.pos.y + 1.5, motor_2.pos.z),
+    rod3_4: vpy.cylinder = vpy.cylinder(pos=vpy.vector(motor_2.pos.x + 6 + 3, motor_2.pos.y + 1.5, motor_2.pos.z),
                                         radius=0.2,
                                         axis=vpy.vector(3, 0, 0))
-    motor_3: vpy.cylinder = vpy.cylinder(pos=vpy.vector(6, -1.5, 0), radius=0.3, axis=vpy.vector(0, 3, 0))
+    motor_3: vpy.cylinder = vpy.cylinder(pos=vpy.vector(motor_2.pos.x + 6, -1.5, 0), radius=0.3, axis=vpy.vector(0, 3, 0))
 
     part_2: vpy.compound = vpy.compound([motor_2, rod2_4, rod2_3, rod2_1, rod2_2],
-                                        origin=vpy.vector(0, 0, 2.5))
+                                        origin=vpy.vector(0, 0, 2.5) )
 
     part_2.color = vpy.color.blue
-    part_2.pos.z += 2.5
+    # part_2.pos.z += 2.5
     part_3: vpy.compound = vpy.compound([motor_3, rod3_4, rod3_3, rod3_1, rod3_2],
-                                        origin=vpy.vector(0, 0, 2.5))
+                                        origin=vpy.vector(6, 0, 2.5) )
     part_3.color = vpy.color.green
-    part_3.pos.x = part_2.pos.x + 3
-    part_3.pos.z = part_2.pos.z
+    # part_3.pos.x = part_2.pos.x + 3
+    # part_3.pos.z = part_2.pos.z
 
-
-    part_2.rotate(angle=np.radians(-90), axis=vpy.vector(0, 0, 1))
     end_x_label = vpy.label(text="PART_2 POS",
                             color=vpy.color.cyan,
                             pos=part_2.pos + vpy.vector(0.025, 0, 0),
@@ -193,7 +196,27 @@ if __name__ == "__main__":
         try:
             if keyboard.is_pressed('a'):
                 THETA_1 -= THETA_UNIT
-                if (THETA_1 + 360 == 0):
+                if THETA_1 + 360 == 0:
+                    THETA_1 = 0
+
+                print(f'Theta 1 = {THETA_1}')
+                homogeneous_matrix = full_homo_matrix(2)
+                print(f'Homo matrix for 1 to 2 = {homogeneous_matrix}')
+
+                # redraw(homogeneous_matrix, part_2,
+                #        vpy.vector(homogeneous_matrix[0][3], homogeneous_matrix[1][3], homogeneous_matrix[2][3]))
+                redraw(homogeneous_matrix, part_2, vpy.vector(0, 0, 2.5), 2)
+
+                homogeneous_matrix = full_homo_matrix(3)
+                print(f'Homo matrix for 1 to 3 = {homogeneous_matrix}')
+
+                redraw(homogeneous_matrix, part_3,
+                       vpy.vector(0, 0, 2.5), 3)
+                # redraw(homogeneous_matrix, part_3, vpy.vector(6 * np.cos(THETA_2), 6 * np.sin(THETA_2), 2.5))
+
+            elif keyboard.is_pressed('s'):
+                THETA_1 += THETA_UNIT
+                if THETA_1 + 360 == 0:
                     THETA_1 = 0
 
                 print(f'Theta 1 = {THETA_1}')
@@ -205,16 +228,8 @@ if __name__ == "__main__":
                 redraw(homogeneous_matrix, part_2, vpy.vector(0, 0, 2.5), 2)
 
                 homogeneous_matrix = full_homo_matrix(3)
-                # print(f'Homo matrix for 1 to 3 = {homogeneous_matrix}')
-
                 redraw(homogeneous_matrix, part_3,
                        vpy.vector(0, 0, 2.5), 3)
-                # redraw(homogeneous_matrix, part_3, vpy.vector(6 * np.cos(THETA_2), 6 * np.sin(THETA_2), 2.5))
-
-            # elif keyboard.is_pressed('s'):
-            #     print('Rotating joint 1 10 degrees clock-wise')
-            #     homogeneous_matrix = calc_homo_matrix(THETA_UNIT, 0)
-            #     re_draw_new_frame()
             # 
             # 
             # elif keyboard.is_pressed('q'):
